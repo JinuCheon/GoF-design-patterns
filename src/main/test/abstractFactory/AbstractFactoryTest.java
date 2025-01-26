1e955f954dfcb9d8eab412f5c598cc5bc0147a51
JinuCheon <woojin8787@gmail.com>
2025-01-26 17:18:06 +0900

    add check list

---
+++ src/main/test/abstractFactory/AbstractFactoryTest.java
@@ -0,0 +1,8 @@
+package main.test.abstractFactory;
+
+/// - [] 새벽배송은 보온 박스에 포장된다
+/// - [] 다음날배송은 골판지 박스에 포장된다
+/// - [] 새벽배송은 "Dawn Delivery" 라는 라벨이 출력된다
+/// - [] 다음날배송은 "Next Day Delivery" 라는 라벨이 출력된다
+public class AbstractFactoryTest {
+}
