c4b270d1ddef4f6a9a4086269f957cb926ed8f5c
JinuCheon <woojin8787@gmail.com>
2025-01-26 22:00:40 +0900

    [Red] 삼각측량: stubbing 을 깨지는 조건을 추가

--- src/main/test/abstractFactory/AbstractFactoryTest.java
+++ src/main/test/abstractFactory/AbstractFactoryTest.java
@@ -5,17 +5,19 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-/// - [] 새벽배송은 보온 박스에 포장된다
+/// - [v] 새벽배송은 보온 박스에 포장된다
 /// - [] 다음날배송은 골판지 박스에 포장된다
 /// - [] 새벽배송은 "Dawn Delivery" 라는 라벨이 출력된다
 /// - [] 다음날배송은 "Next Day Delivery" 라는 라벨이 출력된다
 public class AbstractFactoryTest {
 
     @Test
-    @DisplayName("새벽배송은 보온 박스에 포장된다")
+    @DisplayName("새벽배송은 보온 박스에 포장되며, 다음날 배송은 골판지 박스에 포장된다")
     void dawn_delivery_should_be_packaged_in_hermal_box() {
-        PackingClient packingClient = new PackingClient(new DawanDeliveryPackingFactory());
+        PackingClient dawnDeliveryPackingClient = new PackingClient(new DawanDeliveryPackingFactory());
+        PackingClient nextDayDeliveryPackingClient = new PackingClient(new NextDayDeliveryPackingFactory());
 
-        assertEquals(packingClient.boxType(), "Hermal Box");
+        assertEquals(dawnDeliveryPackingClient.boxType(), "Hermal Box");
+        assertEquals(nextDayDeliveryPackingClient.boxType(), "Cardboard Box");
     }
 }
